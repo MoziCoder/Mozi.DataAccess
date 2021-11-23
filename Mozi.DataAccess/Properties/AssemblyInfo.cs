c1acbea3bd243b122ec0a36e92073a305c6500ef
Jason <brotherqian@163.com>
2021-11-23 10:37:19 +0800

    修改Readme

--- Mozi.DataAccess/Properties/AssemblyInfo.cs
+++ Mozi.DataAccess/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Mozi.DataAccess")]
-[assembly: AssemblyDescription("Mozi.DataAccess核心运行组件")]
+[assembly: AssemblyDescription("Mozi.DataAccess核心组件")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MoziCoder workgroup")]
 [assembly: AssemblyProduct("Mozi.DataAccess")]
