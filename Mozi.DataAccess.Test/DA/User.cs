32d146f8bc61ffac3cfb6e9175e431c6b790912c
Jason <brotherqian@163.com>
2022-05-31 14:26:48 +0800

    增加访问类

---
+++ Mozi.DataAccess.Test/DA/User.cs
@@ -0,0 +1,4 @@
+namespace Mozi.DataAccess.Test.DA
+{
+
+}
---
+++ Mozi.DataAccess.Test/Model/User.cs
@@ -0,0 +1,13 @@
+
+namespace Mozi.DataAccess.Test.Model
+{
+	public class User
+    {
+        public string UserId    { get; set; }
+        public string NickName  { get; set; }
+        public string Password  { get; set; }
+        public string Mobile    { get; set; }
+        public DateTime RegDate { get; set; }
+        public int IsForbidden  { get; set; }
+    }
+}
